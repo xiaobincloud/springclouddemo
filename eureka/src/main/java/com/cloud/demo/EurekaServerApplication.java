da64d5e4349975944452610942b1838c0d9862d3
xiaobincloud <32972490+xiaobincloud@users.noreply.github.com>
2018-06-06 09:21:38 +0800

    注册中心完成

---
+++ eureka/src/main/java/com/cloud/demo/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xiaobin
+ * 成都友漾健康科技有限公司
+ * Created by xiaobin on 2018/6/6
+ * Copyright (c) 2016-2019
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
